cb1381b81edd4eb5eaf4ffa0626196c04e1c2a72
murphy-afk <bardarenoemi@gmail.com>
2025-12-01 15:29:29 +0100

    feat: article data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { articles } from './assets/articleData'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1>test</h1>
+      <h1>Articles</h1>
+      <ul>
+        {articles.map(article => {
+          return (
+            <li>{article}</li>
+          )
+        })}
+      </ul>
     </>
   )
 }
---
+++ src/assets/articleData.js
@@ -0,0 +1,13 @@
+const articles = ["Unlocking the Future of Renewable Energy",
+  "How AI Is Transforming Everyday Life",
+  "The Hidden Psychology Behind Consumer Behavior",
+  "Top 10 Travel Destinations for Digital Nomads",
+  "What Quantum Computing Means for the Next Decade",
+  "A Beginner's Guide to Personal Finance",
+  "The Rise of Remote Work Culture",
+  "Exploring the Science of Sleep",
+  "Why Sustainable Fashion Is More Important Than Ever",
+  "The Art of Effective Communication in the Workplace"
+]
+
+export { articles }
