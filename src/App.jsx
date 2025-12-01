b0dcdc64315844ddd92bc31481a9aa1366a30552
murphy-afk <bardarenoemi@gmail.com>
2025-12-01 15:37:12 +0100

    feat: form structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,31 @@ import { useState } from 'react'
 import './App.css'
 import { articles } from './assets/articleData'
 
+function handleChange() {
+  return 
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [newArticle, setNewArticle] = useState('new article')
 
   return (
     <>
       <h1>Articles</h1>
+      {/* article list  */}
       <ul>
-        {articles.map(article => {
+        {articles.map((article, index) => {
           return (
-            <li>{article}</li>
+            <li key={index}>{article}</li>
           )
         })}
       </ul>
+      {/* new article form  */}
+      <form action="submit">
+        <label htmlFor="newTitle">
+          Input new article title
+        </label>
+        <input id='newTitle' type="text" value={newArticle} onChange={handleChange}/>
+      </form>
     </>
   )
 }
